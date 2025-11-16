bcdffc1ac63019cb72b1a9af16d39d693d6eb4fc
Tom Doerffel <doerffel@iap-kborn.de>
2025-11-16 18:52:19 +0100

    add type conversion tests

--- tests/python/test_python_lib.py
+++ tests/python/test_python_lib.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from swami import MCM
-
+from swami import MCMOutput
 
 def test_instantiation():
 
@@ -20,6 +20,29 @@ def test_instantiation():
     res = mcm.run(
         al, doy, lt, lat, lon, f107, f107a, kp1, kp2)
 
-
     # Check if all values are returned
     assert np.all(np.isfinite(mcm.mcm_output_to_array(res)))
+
+
+def test_array_to_mcm_output():
+
+    data_out_array = np.linspace(1, 17, 17)
+    data_in_array = np.linspace(-1, -9, 9)
+
+    mcm_ref = MCMOutput(*data_out_array, *data_in_array)
+
+    mcm_out = MCM.array_to_mcm_output(data_in_array, data_out_array)
+
+    assert mcm_ref == mcm_out
+
+
+def test_mcm_output_to_array():
+
+    data_out_array = np.linspace(1, 17, 17)
+    data_in_array = np.linspace(-1, -9, 9)
+
+    mcm_out = MCMOutput(*data_out_array, *data_in_array)
+
+    data_out_array_ref = MCM.mcm_output_to_array(mcm_out)
+
+    np.testing.assert_allclose(data_out_array, data_out_array_ref)
