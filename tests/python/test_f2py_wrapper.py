4d23f0df97d6af165333d89df9e16b87cb34f9b2
Tom Doerffel <doerffel@iap-kborn.de>
2025-11-16 18:05:10 +0100

    further extent test

--- tests/python/test_f2py_wrapper.py
+++ tests/python/test_f2py_wrapper.py
@@ -5,3 +5,23 @@ import os
 import swami
 from swami.mcm_wrapper import mcm_wrapper
 
+
+def test_instantiation():
+
+    doy = 1.
+    lt = 16.
+    al = 250.
+    lat = 60.
+    lon = 60.
+    f107 = 150.
+    f107a = 150.
+    kps = 6., 6.
+
+    data_um = os.path.dirname(swami.__file__) + "/data/um/"
+    data_dtm = os.path.dirname(swami.__file__) + "/data/"
+
+    res = mcm_wrapper(
+        doy, lt, al, lat, lon, f107, f107a, kps, data_um, data_dtm)
+
+    # Check if all values are returned
+    assert np.all(np.isfinite(res))
