95219cea26d6cb28e3ab044c2a3da33f09143aea
Tom Doerffel <doerffel@iap-kborn.de>
2025-11-16 18:54:54 +0100

    major re-write of the MCM class

--- src/swami/__init__.py
+++ src/swami/__init__.py
@@ -12,18 +12,18 @@
 # – All rights reserved
 # ---------------------------------------------------------------------
 
+import numpy as np
 import os
-import subprocess
-import tempfile
 from pathlib import Path
-from typing import Dict, NamedTuple
+from typing import NamedTuple
+
+from swami.mcm_wrapper import mcm_wrapper
 
 __version__ = "swami-1.0.rc"
 
 _PWD = Path(__file__).absolute().parent
 _NONE = -999999
 
-_PATH_DEFAULT_EXEC = _PWD / "swami.x"
 _PATH_DEFAULT_DATA = _PWD / "data"
 
 
@@ -66,7 +66,6 @@ class MCM:
         path_to_data (os.PathLike, optional): Path to the data. Defaults to the included package.
     """
 
-    path_to_bin: Path = _PATH_DEFAULT_EXEC
     path_to_data: Path = _PATH_DEFAULT_DATA
 
     def __init__(
@@ -87,64 +86,21 @@ class MCM:
             self.path_to_data = Path(path_to_data)
 
     @staticmethod
-    def _generate_nml_from_dict(
-        d: Dict[str, int | float | bool | str], name: str = "input"
-    ):
-        """Generate a namelist file from a dictionary
+    def array_to_mcm_output(input: np.ndarray, output: np.ndarray) -> MCMOutput:
 
-        Args:
-            d (dict): Dictionary
-            name (str, optional): Name of the namelist. Defaults to "input".
-        """
+        mcm_output = MCMOutput(
+            *output, *input
+        )
 
-        def logical(b: bool):
-            return ".true." if b else ".false."
-
-        with tempfile.NamedTemporaryFile(
-            prefix="swami_", delete=False, suffix=".inp", mode="r+"
-        ) as nml:
-            _ = nml.write(f"&{name}\n")
-            for k, v in d.items():
-                # Booleans
-                if isinstance(v, bool):
-                    _ = nml.write(f"{k} = {logical(v):s}\n")
-                # Strings
-                elif isinstance(v, str):
-                    _ = nml.write(f"{k} = '{v:s}'\n")
-                # Floats
-                elif isinstance(v, float):
-                    _ = nml.write(f"{k} = {v:23.16e}\n")
-                # Integers
-                elif isinstance(v, int):  # pyright: ignore[reportUnnecessaryIsInstance]
-                    _ = nml.write(f"{k} = {v:d}\n")
-                # Others
-                else:
-                    _ = nml.write(f"{k} = {v}\n")
-            # nml.write("\\")
-            _ = nml.write("&end\n")
-            nml.close()
-
-            return nml.name
+        return mcm_output
 
     @staticmethod
-    def _read_output_file(outfile: Path | str):
-        """Read output file from swami.x
+    def mcm_output_to_array(
+        mcm_output: MCMOutput) -> np.ndarray:
 
-        Args:
-            outfile (os.PathLike): Path to output file
+        dict_output = mcm_output._asdict()
 
-        Returns:
-            dict: Dictionary with values per model
-        """
-        with open(outfile, mode="r") as f:
-            res = {}
-            for line in f:
-                var, val = line.split("=")
-                val = float(val)
-                val = None if val == _NONE else val
-                res[var.strip()] = val
-
-            return MCMOutput(**res)
+        return np.array([val for val in dict_output.values()])
 
     def run(
         self,
@@ -181,47 +137,27 @@ class MCM:
             MCMOutput: NamedTuple with the results
         """
 
-        # Make temporary output file
-        output_file = tempfile.NamedTemporaryFile(
-            delete=False, suffix=".out", prefix="swami_", mode="r+"
-        )
-
         # Sanitize paths
         data_dtm = str(self.path_to_data)
         data_dtm = data_dtm + "/" if data_dtm[-1] != "/" else data_dtm
         data_um = str(os.path.join(self.path_to_data, "um"))
         data_um = data_um + "/" if data_um[-1] != "/" else data_um
 
-        # Create dictionary with input parameters
-        input_dict = {
-            "altitude": float(altitude),
-            "day_of_year": float(day_of_year),
-            "local_time": float(local_time),
-            "latitude": float(latitude),
-            "longitude": float(longitude),
-            "f107": float(f107),
-            "f107m": float(f107m),
-            "kp1": float(kp1),
-            "kp2": float(kp2),
-            "b_unc_std": bool(get_uncertainty),
-            "b_winds": bool(get_winds),
-            "data_dtm": data_dtm,
-            "data_um": data_um,
-            "output_file": str(output_file.name),
-        }
+        out = mcm_wrapper(
+            day_of_year,
+            local_time,
+            altitude,
+            latitude,
+            longitude,
+            f107,
+            f107m,
+            (kp1, kp2),
+            data_um,
+            data_dtm)
 
-        # Generate input file
-        input_file = self._generate_nml_from_dict(input_dict)
+        input = np.array([
+            altitude, day_of_year, local_time, latitude, longitude, f107, f107m, kp1 ,kp2])
 
-        # Run command
-        cmd = [str(self.path_to_bin), input_file]
-        _ = subprocess.run(cmd, check=True)
+        mcm_out = self.array_to_mcm_output(input, out)
 
-        # Read output file
-        out = self._read_output_file(output_file.name)
-
-        # Delete temporary files
-        os.unlink(input_file)
-        os.unlink(output_file.name)
-
-        return out
+        return mcm_out
