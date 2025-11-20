9b32fab3d2ca6dccb8caa7369932c4033251b1f7
Tom Doerffel <doerffel@iap-kborn.de>
2025-11-20 06:13:44 +0100

    update test

--- tests/python/test_f2py_wrapper.py
+++ tests/python/test_f2py_wrapper.py
@@ -29,18 +29,17 @@ def test_instantiation_mcm():
 
 def test_instantiation_dtm():
 
-    doy = 1.
-    lt = 16.
-    al = 250.
-    lat = 60.
-    lon = 60.
-    f107 = 150.
-    f107a = 150.
-    kps = 6.
+    doy = 1. # day of year
+    lt = 16. # local time in hours
+    al = 250. # height in km
+    lat = 60. # latitude in deg
+    lon = 60.  # longitude in deg
+    f107 = 150. # F10.7 index
+    f107a = 150. # F10.7 index (averaged)
+    kps = 6. # Kp index
 
     res = dtm(
         doy, lt, al, lat, lon, (f107, 0.), (f107a, 0.), (kps, 0., 0., 0.))
 
     # Check if all values are returned
     assert np.all(np.isfinite(res))
-
